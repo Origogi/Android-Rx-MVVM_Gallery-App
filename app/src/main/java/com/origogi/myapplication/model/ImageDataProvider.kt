ca0401389bf60636f42a84d98833859bb4218a59
김정태(Jeongtae Kim)/App개발팀/11ST <origogi.kim@sk.com>
2020-12-11 21:09:20 +0900

    change rxjava3 to rxjava2

--- app/src/main/java/com/origogi/myapplication/model/ImageDataProvider.kt
+++ app/src/main/java/com/origogi/myapplication/model/ImageDataProvider.kt
@@ -1,9 +1,9 @@
 package com.origogi.myapplication.model
 
-import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.subjects.PublishSubject
+
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
 import org.jsoup.Jsoup
-import java.io.IOException
 
 object ImageDataProvider {
 
@@ -19,17 +19,17 @@ object ImageDataProvider {
                 val doc = Jsoup.connect("https://www.gettyimagesgallery.com/collection/sasha/").get()
 
                 val imageItems = doc.select("div[class=grid-item image-item col-md-4]")
-                val imageDataList = mutableListOf<ImageData>()
 
-                imageItems.forEach { imageData ->
+                val imageDataList = imageItems.map { imageData ->
                     val title = imageData.attr("data-title")
-                    val imageUrl = imageData.select("img[class=jq-lazy]")[0].attr("data-src").toString()
-                    imageDataList.add(ImageData(title, imageUrl))
-                }
+                    val imageUrl = imageData.select("img[class=jq-lazy]")[0].attr("data-src")
+                    ImageData(title, imageUrl)
+                }.filter { it.imageUrl.isNotEmpty() }
+                    .toList()
 
                 subject.onNext(imageDataList)
 
-            } catch (e: IOException) {
+            } catch (e: Exception) {
                 subject.onNext(emptyList())
             }
         }.start()
